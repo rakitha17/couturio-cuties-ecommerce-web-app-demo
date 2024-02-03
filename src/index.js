4cc99890cbf16185f4b1e18f24d855e165cedfdc
rakitha17 <rakithabimsara505@gmail.com>
2024-02-03 20:13:55 +0530

    data persisting with redux completed

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import reportWebVitals from "./reportWebVitals"
 //LINK - packages
 import { Provider } from "react-redux"
 import store from "./store/store"
+import { PersistGate } from "redux-persist/integration/react"
+import { persistStore } from "redux-persist"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
+let persistor = persistStore(store)
+
 root.render(
   //<React.StrictMode>
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   //</React.StrictMode>
 )
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,22 @@
 import { applyMiddleware, legacy_createStore } from "redux"
 // import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly"
 import { thunk } from "redux-thunk"
+import storage from "redux-persist/lib/storage"
+import { persistReducer } from "redux-persist"
 //LINK - sources
 import rootReducer from './reducers/rootReducer'
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = legacy_createStore(
-  rootReducer,
+  // rootReducer,
+  persistedReducer,
   applyMiddleware(thunk)
   // composeWithDevTools(applyMiddleware(thunk))
 )
