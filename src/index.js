c1edc5981674b4ba48bed93c228bee205084f878
rakitha17 <rakithabimsara505@gmail.com>
2024-02-09 23:06:19 +0530

    service worker and manifest are created

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import reportWebVitals from "./reportWebVitals"
 //LINK - packages
 import { Provider } from "react-redux"
 import store from "./store/store"
 import { PersistGate } from "redux-persist/integration/react"
 import { persistStore } from "redux-persist"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from "./reportWebVitals"
 //LINK - utilities
 import './i18next'
 
@@ -15,14 +16,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 let persistor = persistStore(store)
 
 root.render(
-  //<React.StrictMode>
+  <React.StrictMode>
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
-  //</React.StrictMode>
+  </React.StrictMode>
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
